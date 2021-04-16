987e947df81af59b97bbd86dac6eafff7cfb0b08
davisouzamoya <davismoya01@gmail.com>
2021-04-15 23:37:59 -0300

    teste

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl:true,
     migrations: {
         directory: './src/database/migrations'
     }
