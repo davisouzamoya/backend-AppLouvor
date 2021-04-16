2a3b394f2a9d14b094b60deec1c9287a511dfd9a
davisouzamoya <davismoya01@gmail.com>
2021-04-15 23:19:25 -0300

    teste

--- knexfile.js
+++ knexfile.js
@@ -1,42 +1,22 @@
 // Update with your config settings.
 
 module.exports = {
-
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: './src/database/migrations'
+    }
+},
   development: {
     client: 'pg',
-    connection: 'postgres://dzeqfhzdsjivzv:397ea716567017b157e3706e8fd8ec3f826b136875dca0f088a49be8c30bce9d@ec2-18-233-83-165.compute-1.amazonaws.com:5432/ddkfo5ttqbjup0', 
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
     migrations: {
       directory: './src/database/migrations'
-    },
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: 'postgres://dzeqfhzdsjivzv:397ea716567017b157e3706e8fd8ec3f826b136875dca0f088a49be8c30bce9d@ec2-18-233-83-165.compute-1.amazonaws.com:5432/ddkfo5ttqbjup0', 
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: 'postgres://dzeqfhzdsjivzv:397ea716567017b157e3706e8fd8ec3f826b136875dca0f088a49be8c30bce9d@ec2-18-233-83-165.compute-1.amazonaws.com:5432/ddkfo5ttqbjup0', 
-    // connection: {
-      // database: 'my_db',
-      // user:     'username',
-      // password: 'password'
-    // },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   }
-
 };
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,7 @@
 const knex = require('knex')
-const configuration = require('../../knexfile')
+const knexConfig = require('../../knexfile')
+const environment = process.env.DB_ENV || 'development';
+// const connection = knex(configuration.development)
 
-const connection = knex(configuration.development)
-
-module.exports = connection;
+// module.exports = connection;
+module.exports = knex(knexConfig[environment]);
