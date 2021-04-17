672c625ea4ecc461349c22d87000793ca321721f
davisouzamoya <davismoya01@gmail.com>
2021-04-17 01:35:00 -0300

    asd

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,10 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: false
+      connectionString : process.env.DATABASE_URL
+    },
+    ssl: {
+      rejectUnauthorized: false
     },
     migrations: {
       tableName: 'knex_migrations',
